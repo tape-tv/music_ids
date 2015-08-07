c52eee62c8ec5e9c98cbe993fdf39b35fb3d959f
Matt Patterson <matt@reprocessed.org>
2015-08-07 15:35:08 +0200

    Add simple ISRC parsing and equality test

--- lib/music_ids/isrc.rb
+++ lib/music_ids/isrc.rb
@@ -1,5 +1,17 @@
 module MusicIds
   class ISRC
+    def self.parse(input)
+      input = input.to_s
+
+      if input.length == 12
+        new(input)
+      elsif input.length == 15
+        new(input.gsub('-', ''))
+      else
+        raise ArgumentError, "'#{input}' is not the right length to be a 12- or 15-character ISRC"
+      end
+    end
+
     def initialize(isrc_string)
       @isrc_string = isrc_string.dup.freeze
     end
@@ -22,6 +34,14 @@ module MusicIds
 
     def to_s
       @isrc_string.dup
+    end
+
+    def to_isrc
+      self
+    end
+
+    def ==(other)
+      to_s == other.to_s
     end
   end
 end
--- spec/music_ids/isrc_spec.rb
+++ spec/music_ids/isrc_spec.rb
@@ -1,33 +1,61 @@
 require 'music_ids/isrc'
 
-RSpec.describe MusicIds::ISRC do
-  context "an instance" do
-    let(:isrc) { MusicIds::ISRC.new("FRZ039800212") }
+module MusicIds
+  RSpec.describe ISRC do
+    context "an instance" do
+      let(:isrc) { ISRC.new("FRZ039800212") }
 
-    it "reports the country code" do
-      expect(isrc.country).to eq("FR")
-    end
-
-    it "reports the registrant code" do
-      expect(isrc.registrant).to eq("Z03")
-    end
-
-    it "reports the year of reference" do
-      expect(isrc.year).to eq("98")
-    end
-
-    it "reports the designation code" do
-      expect(isrc.designation).to eq("00212")
-    end
-
-    context "returning a string" do
-      it "reports the full isrc string" do
-        expect(isrc.to_s).to eq("FRZ039800212")
+      it "reports the country code" do
+        expect(isrc.country).to eq("FR")
       end
 
-      it "returns a copy of the string so it remains immutable" do
-        mutated = isrc.to_s << "mutated"
-        expect(mutated).to_not eq(isrc.to_s)
+      it "reports the registrant code" do
+        expect(isrc.registrant).to eq("Z03")
+      end
+
+      it "reports the year of reference" do
+        expect(isrc.year).to eq("98")
+      end
+
+      it "reports the designation code" do
+        expect(isrc.designation).to eq("00212")
+      end
+
+      context "returning a string" do
+        it "reports the full isrc string" do
+          expect(isrc.to_s).to eq("FRZ039800212")
+        end
+
+        it "returns a copy of the string so it remains immutable" do
+          mutated = isrc.to_s << "mutated"
+          expect(mutated).to_not eq(isrc.to_s)
+        end
+      end
+
+      it "returns itself when to_isrc called" do
+        expect(isrc.to_isrc).to be(isrc)
+      end
+
+      it "compares equal with itself" do
+        expect(isrc).to eq(isrc)
+      end
+
+      it "compares equal with another ISRC instance of the same ISRC string" do
+        expect(isrc).to eq(ISRC.new(isrc.to_s))
+      end
+    end
+
+    context "parsing" do
+      it "can parse a hyphen-separated ISRC" do
+        expect(ISRC.parse("FR-Z03-98-00212")).to eq(ISRC.new("FRZ039800212"))
+      end
+
+      it "can parse a non-separated ISRC" do
+        expect(ISRC.parse("FRZ039800212")).to eq(ISRC.new("FRZ039800212"))
+      end
+
+      it "will not parse a string that's not long enough" do
+        expect { ISRC.parse("FRZ03") }.to raise_error(ArgumentError)
       end
     end
   end
